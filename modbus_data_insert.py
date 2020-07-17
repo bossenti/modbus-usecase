626591fb80c344917cc0e3385056e1bffa23a476
Tim <udeho@student.kit.edu>
2020-07-17 17:07:05 +0200

    document code properly

--- modbus_data_insert.py
+++ modbus_data_insert.py
@@ -2,12 +2,28 @@ import pandas as pd
 import time
 from pymodbus.client.sync import ModbusTcpClient
 
+"""
+    short script that writes the values of the flow data frame to Modbus registers
+    Each value is updated after one second
+"""
+
 if __name__ == "__main__":
+
+    # read in flow data from csv
     df_data = pd.read_csv("./data/flow.csv", sep=';')
+
+    # drop data that are not of interest for this use case
     df_data.drop(['timestamp', 'sp_internal_label', 'sensorId', 'time'], axis=1, inplace=True)
 
+    # start a Modbus Client on localhost
     client = ModbusTcpClient('localhost')
+
+    # iterate over each row of the data frame
     for idx, row in df_data.iterrows():
+
+        # write each value to the holding register, using another address for each column
         for no, value in enumerate(row):
             client.write_register(no + 1, int(value))
+
+        # wait one second to overwrite values
         time.sleep(1)
--- modbus_slave.py
+++ modbus_slave.py
@@ -1,23 +1,16 @@
-#!/usr/bin/env python
-"""
-Pymodbus Synchronous Server
---------------------------------------------------------------------------
-
-The synchronous server is implemented in pure python without any third
-party libraries (unless you need to use the serial protocols which require
-pyserial). This is helpful in constrained or old environments where using
-twisted is just not feasible. What follows is an example of its use:
-"""
 from pymodbus.server.sync import StartTcpServer
-
 from pymodbus.device import ModbusDeviceIdentification
 from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
-
-# --------------------------------------------------------------------------- #
-# configure the service logging
-# --------------------------------------------------------------------------- #
 import logging
+
+"""
+    This script provides the Modbus device
+    Implementation is based on 
+    https://pymodbus.readthedocs.io/en/latest/source/example/synchronous_server.html
+"""
+
+# configure the logging
 FORMAT = ('%(asctime)-15s %(threadName)-15s'
           ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
 logging.basicConfig(format=FORMAT)
@@ -25,12 +18,16 @@ log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
 
-def run_server(num_fields):
-    # The slave context can also be initialized in zero_mode which means that a
-    # request to address(0-7) will map to the address (0-7). The default is
-    # False which is based on section 4.4 of the specification, so address(0-7)
-    # will map to (1-8)::
+def run_server(ip_address, num_fields, port=502):
+    """
+    Initiates a modbus server (device)
+    :param ip_address: ip_adress under which the Modbus device should be accessible
+    :param port: port to which the server listens
+    :param num_fields: length of the registers
+    :return: None
+    """
 
+    # configure the device and initialize the registers
     store = ModbusSlaveContext(
         di=ModbusSequentialDataBlock(0, [0]*num_fields),
         co=ModbusSequentialDataBlock(0, [0]*num_fields),
@@ -46,8 +43,8 @@ def run_server(num_fields):
     identity.ModelName = 'Modbus Slave'
     identity.MajorMinorRevision = '2.3.0'
 
-    StartTcpServer(context, identity=identity, address=("localhost", 502))
+    StartTcpServer(context, identity=identity, address=(ip_address, port))
 
 
 if __name__ == "__main__":
-    run_server(10)
+    run_server("localhost", 502, 10)
